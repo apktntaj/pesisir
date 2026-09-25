2e67b1ddcd91913125d86e7e4ed8eacbd7678f17
apktntaj <maiaelarsya@gmail.com>
2026-09-25 14:47:44 +0700

    chore: restore Next.js runtime workspace

--- apps/workspace/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { mount } from 'svelte'
-import App from './App.svelte'
-import './app.css'
-
-const target = document.getElementById('app')
-if (!target) throw new Error('Application root tidak ditemukan.')
-
-mount(App, { target })
